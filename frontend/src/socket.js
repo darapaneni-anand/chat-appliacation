26212485a8092cad7b751e4906cafb0edb888284
darapaneni-anand <166981472+darapaneni-anand@users.noreply.github.com>
2025-09-06 23:58:44 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,7 @@
-// src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_API_URL, {
-  withCredentials: true,
+const socket = io("https://chat-application-2-3ne8.onrender.com", {
+  withCredentials: true, // optional
 });
 
 export default socket;
