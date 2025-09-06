2f36bc12b99dd6d1ee98a8b121ddf08f17e066b1
darapaneni-anand <166981472+darapaneni-anand@users.noreply.github.com>
2025-09-06 23:46:59 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,8 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  withCredentials: true, // optional
+const socket = io(process.env.REACT_APP_BACKEND_URL, {
+  withCredentials: true,
 });
 
 export default socket;
