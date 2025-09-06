e895986d2f8471e268f00a522604f87eff25fbf2
darapaneni-anand <166981472+darapaneni-anand@users.noreply.github.com>
2025-09-06 23:41:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -90,13 +90,7 @@ io.on("connection", (socket) => {
   });
 });
 
-// Serve React frontend build for production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-  });
-}
+
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
